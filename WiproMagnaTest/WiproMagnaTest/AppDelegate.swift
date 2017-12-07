7c7a59dc19b9ca19d6fa36e3054ac52eba72def9
SDRathod <samir3011rathod@gmail.com>
2017-12-07 15:42:12 +0530

    Create class FactsVC for listing in tableview and set root view in navigation from AppDelegate

--- WiproMagnaTest/WiproMagnaTest/AppDelegate.swift
+++ WiproMagnaTest/WiproMagnaTest/AppDelegate.swift
@@ -11,12 +11,23 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-
+    var navObj: UINavigationController?
+    
+    // MARK: -  Did Finish Launching Method
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow.init(frame: UIScreen.main.bounds)
+        
+        let factsObj = FactsVC()
+        navObj = UINavigationController.init(rootViewController: factsObj)
+        window?.rootViewController = navObj;
+        
+        window?.makeKeyAndVisible()
         
         return true
     }
 
+    // MARK: -  AppDelegate Life Cycle Methods
     func applicationWillResignActive(_ application: UIApplication) {
     }
 
---
+++ WiproMagnaTest/WiproMagnaTest/Controller/FactsVC.swift
@@ -0,0 +1,21 @@
+//
+//  FactsVC.swift
+//  WiproMagnaTest
+//
+//  Created by Samir Rathod on 07/12/17.
+//  Copyright © 2017 Samir Rathod. All rights reserved.
+//
+
+import UIKit
+
+class FactsVC: UITableViewController {
+    
+    // MARK: -  View Controller Life Cycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
