8f658151e11d9f6327c455ffdf9ee4cd24eb1dfd
SDRathod <samir3011rathod@gmail.com>
2017-12-07 16:06:11 +0530

    API call Method & Display data in tableview

--- WiproMagnaTest/WiproMagnaTest/Controller/FactsVC.swift
+++ WiproMagnaTest/WiproMagnaTest/Controller/FactsVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class FactsVC: UITableViewController {
     var arrFacts: [FactModel] = [FactModel]()
@@ -14,10 +15,63 @@ class FactsVC: UITableViewController {
     // MARK: -  View Controller Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.separatorStyle = .none
+        self.tableView.register(FactCell.self, forCellReuseIdentifier: "FactCell")
+        
+        getFactListCall()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.tableView.estimatedRowHeight = 44
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+}
+
+extension FactsVC {
+    // MARK: -  API Call Method
+    func getFactListCall() {
+        let url = URL.init(string: "\(Global.apiBaseURLPath)facts.json")
+        let urlRequest = URLRequest(url: url!)
+        
+        NSURLConnection.sendAsynchronousRequest(urlRequest, queue: OperationQueue(), completionHandler: {(response: URLResponse?, data: Data?, error: Error?) -> Void in
+            if (error == nil && data != nil) {
+                let strData: String = String.init(data: data!, encoding: .ascii)!
+                let tempData: Data = strData.data(using: .utf8)!
+                
+                let dictData: NSDictionary = try! JSONSerialization.jsonObject(with: tempData, options: .allowFragments) as! NSDictionary
+                
+                self.arrFacts.removeAll()
+                
+                self.title = dictData.object(forKey: "title") as? String ?? ""
+                if let arrData: NSArray = dictData.object(forKey: "rows") as? NSArray {
+                    for i in 0 ..< arrData.count {
+                        if let dictFact: NSDictionary = arrData.object(at: i) as? NSDictionary {
+                            let modelObj = FactModel()
+                            modelObj.strTitle = dictFact.object(forKey: "title") as? String ?? ""
+                            modelObj.strDesc = dictFact.object(forKey: "description") as? String ?? ""
+                            modelObj.strImgURL = dictFact.object(forKey: "imageHref") as? String ?? ""
+                            
+                            self.arrFacts.append(modelObj)
+                        }
+                    }
+                    self.refreshControl?.endRefreshing()
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+            else {
+                self.title = "Error"
+                self.refreshControl?.endRefreshing()
+            }
+        } )
     }
 }
 
@@ -36,6 +90,13 @@ extension FactsVC {
         if cell == nil {
             cell = FactCell(style: .default, reuseIdentifier: "FactCell")
         }
+        
+        let modelObj = arrFacts[indexPath.row]
+        
+        cell?.imgThumb?.sd_setImage(with: URL.init(string: modelObj.strImgURL), placeholderImage: #imageLiteral(resourceName: "imgPlaceholder"))
+        cell?.lblTitle?.text = modelObj.strTitle
+        cell?.lblDesc?.text = modelObj.strDesc
+        
         cell?.sizeToFit()
         
         return cell!
